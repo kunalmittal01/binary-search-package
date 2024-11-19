d0fb887746824c1b89f2e23c7e0f06094b2b3174
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-11-20 01:50:50 +0530

    package created.

---
+++ index.js
@@ -0,0 +1,21 @@
+const binarySearch = (ar,ele)=>{
+    let i = 0, j = ar.length -1; 
+    while(i <= j) {
+        
+        const mid = i + parseInt((j-i)/2);
+
+        if(ar[mid] === ele) {
+            return mid;
+        }
+        else if(ar[mid] < ele) {
+            i = mid + 1;
+        }
+
+        else  {
+            j = mid - 1;
+        }
+    }
+    return -1;
+}
+
+module.exports = binarySearch;
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Binary Search Module
+
+## Method
+
+### binarySearch
+
+expects two parameters :
+
+* Array and
+* Index of the element.
